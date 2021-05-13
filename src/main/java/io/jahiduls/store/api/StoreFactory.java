ff7564a6119e31656f5d62fa5b1ba6f0608b0eac
Jahid <jahid90@users.noreply.github.com>
2021-05-13 12:21:01 +0530

    [api] throw if more than one binding found

--- src/main/java/io/jahiduls/store/api/StoreFactory.java
+++ src/main/java/io/jahiduls/store/api/StoreFactory.java
@@ -8,6 +8,12 @@ public class StoreFactory {
 
         ServiceLoader<StoreProvider> loader = ServiceLoader.load(StoreProvider.class);
 
+        loader.stream().forEach(it -> System.out.println("INFO: found binding " + it.get().getClass().getCanonicalName()));
+
+        if (loader.stream().count() > 1) {
+            throw new RuntimeException("Too many bindings on classpath. Please use just one.");
+        }
+
         StoreProvider storeProvider = loader.findFirst()
                 .orElseThrow(() -> new RuntimeException("No bindings found on classpath."));
 
